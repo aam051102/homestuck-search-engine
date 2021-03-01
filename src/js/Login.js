4c7f12764c2d5c9dae92507d31f89157f0223684
MadCreativity <ahlgreenmadsen@gmail.com>
2021-03-01 14:23:16 +0100

    Improve server and fix HSSE build location.

--- src/js/Login.js
+++ src/js/Login.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 import "../css/Login.scss";
+import Background from "./Background";
 
 import Layout from "./Layout";
 
 function LoginPage() {
-    return <Layout className="login-page" title="Sign In"></Layout>;
+    return (
+        <Layout className="login-page" title="Sign In">
+            <Background src="https://pipe.miroware.io/5bc7665ecc3313406322aba0/dirk_bg_earthC.png" />
+        </Layout>
+    );
 }
 
 export default LoginPage;
