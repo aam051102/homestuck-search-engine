5542cabe21cc1e3ec8a8e0818c7c98a0c05823af
aam051102 <ahlgreenmadsen@gmail.com>
2021-03-29 23:00:22 +0200

    Move background image to local files.

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 
 import Background from "./Background";
+
+import bgImage from "../../images/bg.png";
 import "../../css/Layout.scss";
 
 function Layout(props) {
@@ -11,7 +13,7 @@ function Layout(props) {
                 <title>{props.title}</title>
             </Helmet>
 
-            <Background src="https://pipe.miroware.io/5bc7665ecc3313406322aba0/dirk_bg_earthC.png" />
+            <Background src={bgImage} />
 
             <div className="page-content">{props.children}</div>
         </main>
