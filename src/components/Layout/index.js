394ce26ec3f61d8281908345cd2031d3eb5f4b8d
MadCreativity <ahlgreenmadsen@gmail.com>
2021-10-30 01:12:56 +0200

    Fix Google Analytics.

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { Helmet } from "react-helmet";
 
 import Background from "components/Background";
@@ -8,21 +8,29 @@ import bgImage from "assets/images/bg.png";
 import "./index.scss";
 
 function Layout(props) {
+    useLayoutEffect(() => {
+        const inlineScript = document.createElement("script");
+
+        inlineScript.innerHTML = `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-78420552-1')`;
+
+        document.body.appendChild(inlineScript);
+
+        return () => {
+            document.body.removeChild(inlineScript);
+        };
+    }, []);
+
     return (
         <main className={`main ${props.className || ""}`}>
             <Helmet>
                 <title>{props.title}</title>
-
-                {/* Global site tag (gtag.js) - Google Analytics */}
-                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-78420552-1"></script>
-                <script dangerouslySetInnerHTML={{__html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-
-                    gtag('config', 'UA-78420552-1');
-                    `}} />
             </Helmet>
+
+            {/* Global site tag (gtag.js) - Google Analytics */}
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-78420552-1"></script>
 
             <Background src={bgImage} />
 
