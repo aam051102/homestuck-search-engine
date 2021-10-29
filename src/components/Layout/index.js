baa272745a8b68d87e6c5ab005c2c962e8edf009
MadCreativity <ahlgreenmadsen@gmail.com>
2021-10-30 01:02:37 +0200

    Add Google analytics to HSSE.

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -12,6 +12,16 @@ function Layout(props) {
         <main className={`main ${props.className || ""}`}>
             <Helmet>
                 <title>{props.title}</title>
+
+                {/* Global site tag (gtag.js) - Google Analytics */}
+                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-78420552-1"></script>
+                <script dangerouslySetInnerHTML={{__html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'UA-78420552-1');
+                    `}} />
             </Helmet>
 
             <Background src={bgImage} />
