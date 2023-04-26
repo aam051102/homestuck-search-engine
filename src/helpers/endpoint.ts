333615cef140ccf7fadaadb911c0f27b4dee89ae
MadCreativity <ahlgreenmadsen@gmail.com>
2023-04-26 18:54:10 +0200

    Fix attempt.

--- src/helpers/endpoint.ts
+++ src/helpers/endpoint.ts
@@ -3,7 +3,8 @@ const ENDPOINT =
         ? "http://localhost:4000"
         : "https://ahlgreen.net";
 
-const BASE_URL = window.location.hostname === "localhost" ? "/" : "/app/hsse/";
+//const BASE_URL = window.location.hostname === "localhost" ? "/" : "/app/hsse/";
+const BASE_URL = "/";
 
 export { BASE_URL };
 export default ENDPOINT;
